08d9a7ce96efc1d87908260b798182b03a72e25e
Bruno de Avila Lima <81933936+brav999@users.noreply.github.com>
2022-10-14 14:56:06 -0300

    Delete cotacao_api.py

--- cotacao_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL")
-cotacao = cotacao.json()
-cotacao_dolar = cotacao['USDBRL']['bid']
-cotacao_euro = cotacao['EURBRL']['bid']
-print(cotacao_dolar, cotacao_euro)
